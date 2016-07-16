d4f4ed0174ac215c6e5e569149a762d232ce9551
pavithraponnuvel <pavithraangel61@gmail.com>
2016-07-16 15:17:15 +0530

    Add files via upload

---
+++ stringrev.java
@@ -0,0 +1,7 @@
+public class stringrev {
+    public static void main(String[] args) {
+       String str="pavithra";
+    String s2 ;
+ s2=new StringBuffer(str).reverse().toString(); 
+        System.out.println(""+s2);
+}}
